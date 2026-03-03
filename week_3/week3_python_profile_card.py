cc20b30059764ec8b18d74fa3f5b8d07e3c70568
Awruhabena <lilybampoe63@gmail.com>
2026-03-03 10:12:34 +0000

    profile_card

---
+++ week_3/week3_python_profile_card.py
@@ -0,0 +1,25 @@
+#ID PROFILE CARD
+
+name ="Lily Bampoe"
+age = 25
+country = "Ghana"
+height = "170."
+cohort = "Group 4"
+skill_level = "Beginner"
+favourite_language = "Python"
+
+
+print(f"Name: {name.upper()}")
+print(f"Age: {age}")
+print(f"Country: {country.upper()}")
+print(f"Height: {height.replace('.', 'cm')}")
+print(f"Cohort: {cohort.upper()}")
+print(f"Skill Level: {skill_level.upper()}")
+print(f"Favourite Language: {favourite_language.upper()}")
+
+
+total_weeks = 12
+current_week = 3
+
+weeks_left = total_weeks - current_week
+print(f"Weeks Left in Course: {weeks_left}")
