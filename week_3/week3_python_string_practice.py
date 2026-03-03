26abe5ca6c52425d4edb502b38a7e2d4579a2773
Awruhabena <lilybampoe63@gmail.com>
2026-03-03 10:10:27 +0000

    variables_and_strings

---
+++ week_3/week3_python_string_practice.py
@@ -0,0 +1,30 @@
+#TASK 1
+first_name = "Lily"
+last_name = "Bampoe"
+age = 25
+programming_concept = "String Manipulation"
+
+print(first_name)
+print(last_name)
+print(age)
+print(programming_concept)      
+
+#TASK 2
+full_name = first_name + " " + last_name
+print(full_name)
+print(f"My full name is {full_name}")
+
+#TASK 3
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.count("a"))
+print(full_name.index("L"))
+print(first_name.replace("Lily", "Coder"))
+
+#TASK 4
+print(f"Hello!, my name is {full_name} and I am {age} years old. My favourite programming concept is {programming_concept}.") 
+
+#TASK 5
+print(full_name[0])
+print(full_name[-1])
+print(full_name[0:4])
