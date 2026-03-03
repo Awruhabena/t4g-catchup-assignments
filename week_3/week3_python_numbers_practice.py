eb6ce35b26b84aef29c8c6c0210afabaa2628be8
Awruhabena <lilybampoe63@gmail.com>
2026-03-03 10:11:33 +0000

    integers_floats_and_arithmetic

---
+++ week_3/week3_python_numbers_practice.py
@@ -0,0 +1,36 @@
+#TASK 1
+number1 = 10
+number2 = 5
+number3 = 3.14
+number4 = 2.5
+
+print(f"Addition: {number1 + number2}") # Addition of two numbers
+print(f"Subtraction: {number1 - number2}") # Subtraction of two numbers
+print(f"Multiplication: {number1 * number2}") # Multiplication of two numbers
+print(f"Division: {number1 / number2}") # Division of two numbers
+print(f"Floor Division: {number1 // number2}") # Floor division of two numbers
+print(f"Modulus: {number1 % number2}") # Modulus of two numbers
+print(f"Exponentiation: {number1 ** number2}") # Exponentiation of two numbers
+
+#TASK 2
+length = 7
+width = 3
+area = length * width
+perimeter = 2 * (length + width) 
+
+print(f"A rectangle with length {length} and width {width} has an area of {area} and a perimeter of {perimeter}.")
+
+#TASK 3
+number_1 = 10
+number_2 = 3.24
+
+new_number1 = float(number_1)
+new_number2 = int(number_2)
+
+print(new_number1)
+print(new_number2)
+print(type(number3))
+print(type(number4))
+print(type(length))
+print(type(width))
+
